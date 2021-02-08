74c56184cbdf60db801bfff949c215fe57fbaa14
Christopher Akiki <christopher.akiki@protonmail.com>
2021-02-08 22:59:45 +0100

    Housekeeping

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import tensorflow_hub as hub
+import tensorflow as tf
+from tqdm.notebook import tqdm_notebook
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,3 @@
-import pandas as pd
-import numpy as np
-import tensorflow_hub as hub
-import tensorflow as tf
-from tqdm.notebook import tqdm_notebook
-
 def tf_hub_embedder(df, col, model, batch_size=512):
     dataset = tf.data.Dataset.from_tensor_slices(df[col].values)
     dataset = dataset.batch(batch_size)
